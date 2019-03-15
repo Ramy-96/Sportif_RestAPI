ff5bd6499194af1e48199b382433ec9a2e078e9b
Ramy-96 <rami.znaki222@gmail.com>
2019-03-15 23:59:54 +0100

    use update and delete user routes as middleware in main router

--- api/rootes/index.js
+++ api/rootes/index.js
@@ -1,10 +1,14 @@
 import express from 'express'
 
 import addUser from './user/addUser'
+import deleteUser from './user/deleteUser'
+import updateUser from './user/updateUser'
 
 const { Router } = express;
 const router = Router();
 
 router.use(addUser);
+router.use(deleteUser);
+router.use(updateUser);
 
 export default router
